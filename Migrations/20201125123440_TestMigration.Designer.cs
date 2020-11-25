f28349d136d741bd7a45d427b002a9b3bff4dd80
Toni <antonio.grama10@yahoo.com>
2020-11-25 13:35:54 +0100

    Test update

--- Migrations/20201125123440_TestMigration.Designer.cs
+++ Migrations/20201125123440_TestMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SEP3_T3.Persistance;
 
 namespace SEP3_Tier3.Migrations
 {
     [DbContext(typeof(ShapeAppDbContext))]
-    partial class ShapeAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201125123440_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20201125123440_TestMigration.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SEP3_Tier3.Migrations
+{
+    public partial class TestMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Users_ReceiverId",
+                table: "Notification");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notification_Users_SenderId",
+                table: "Notification");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification");
+
+            migrationBuilder.RenameTable(
+                name: "Notification",
+                newName: "Notifications");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notification_ReceiverId",
+                table: "Notifications",
+                newName: "IX_Notifications_ReceiverId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications",
+                columns: new[] { "SenderId", "ReceiverId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Users_ReceiverId",
+                table: "Notifications",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Users_SenderId",
+                table: "Notifications",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Users_ReceiverId",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Users_SenderId",
+                table: "Notifications");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Notifications",
+                table: "Notifications");
+
+            migrationBuilder.RenameTable(
+                name: "Notifications",
+                newName: "Notification");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Notifications_ReceiverId",
+                table: "Notification",
+                newName: "IX_Notification_ReceiverId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notification",
+                table: "Notification",
+                columns: new[] { "SenderId", "ReceiverId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Users_ReceiverId",
+                table: "Notification",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notification_Users_SenderId",
+                table: "Notification",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
